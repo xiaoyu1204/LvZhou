1f75e37f16aebf2690eee38693cb1cc8a403e1ab
小雨 <lxy18501240085@163.com>
2021-01-20 16:27:07 +0800

    绿洲base

---
+++ KotlinBase/src/main/java/com/example/kotlinbase/api/ServiceApi.kt
@@ -0,0 +1,18 @@
+package com.shop.api
+
+interface ServiceApi {
+
+//    //商品详情底部数据  id=1155000
+//    @GET("goods/related")
+//    suspend fun getDetailInfoBottom(@Query("id")id:Int):BaseResp<DetailInfoBottomData>
+//
+//    //加入购物车
+//    @POST("cart/add")
+//    @FormUrlEncoded
+//    suspend fun AddCar(@FieldMap map: HashMap<String, String>):BaseResp<CarAddBean>
+//
+//    //获取购物车数据
+//    @GET("cart/index")
+//    suspend fun getCar():BaseResp<CarData>
+
+}
